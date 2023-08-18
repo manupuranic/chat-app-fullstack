e687a077c3e3b1a012ce31a89bf9ce32ceb974f5
manupuranic <manupuranic@gmail.com>
2023-08-18 15:09:48 +0530

    deploy ready

--- public/signup/signup.js
+++ public/signup/signup.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://puranic.in";
+const baseUrl = "https://puranic.in";
 const signUpForm = document.getElementById("signUpForm");
 const msg = document.getElementById("message");
 
